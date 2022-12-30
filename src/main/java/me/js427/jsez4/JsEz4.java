fe3992de0ac143982c8efb17d085bfe47a58caca
JS427 <itsjonghyunshin427@gmail.com>
2022-12-31 08:31:11 +0900

    Edited File

--- src/main/java/me/js427/jsez4/JsEz4.java
+++ src/main/java/me/js427/jsez4/JsEz4.java
@@ -3,15 +3,11 @@ package me.js427.jsez4;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class JsEz4 extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
