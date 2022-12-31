9f6cff8869ab63a396ffb6b4c6a56377be73986a
JS427 <itsjonghyunshin427@gmail.com>
2022-12-31 16:54:10 +0900

    Edited File

---
+++ src/main/java/me/js427/jsez4/exceptions/NotEnoughCommandArgumentException.java
@@ -0,0 +1,7 @@
+package me.js427.jsez4.exceptions;
+
+public class NotEnoughCommandArgumentException extends Exception {
+    public NotEnoughCommandArgumentException(String errorMessage) {
+        super(errorMessage);
+    }
+}
