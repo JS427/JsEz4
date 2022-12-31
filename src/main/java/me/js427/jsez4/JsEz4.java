f73b2d358a869b2f8993d09a67395553ed641071
JS427 <itsjonghyunshin427@gmail.com>
2022-12-31 15:39:38 +0900

    Edited File

--- src/main/java/me/js427/jsez4/JsEz4.java
+++ src/main/java/me/js427/jsez4/JsEz4.java
@@ -5,6 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class JsEz4 extends JavaPlugin {
     @Override
     public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
     }
 
     @Override
