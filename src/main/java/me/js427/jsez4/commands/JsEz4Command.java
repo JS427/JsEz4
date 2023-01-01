f6236e5ddb30dff37c0e86beca75d94c32ef5ceb
JS427 <itsjonghyunshin427@gmail.com>
2023-01-01 12:44:08 +0900

    Edited File

--- src/main/java/me/js427/jsez4/commands/JsEz4Command.java
+++ src/main/java/me/js427/jsez4/commands/JsEz4Command.java
@@ -1,5 +1,7 @@
 package me.js427.jsez4.commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +10,10 @@ import org.jetbrains.annotations.NotNull;
 public final class JsEz4Command implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof BlockCommandSender)
+            sender.sendMessage(ChatColor.RED + "Only console, players can execute this command!");
+
+        sender.sendMessage(ChatColor.GREEN + "You are currently running JsEz4 v1.0-SNAPSHOT");
         return true;
     }
 }
