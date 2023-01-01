e09b230953ae5e5ffa2667cc4db54e40cdba2267
JS427 <itsjonghyunshin427@gmail.com>
2023-01-01 09:18:23 +0900

    Edited File

--- src/main/java/me/js427/jsez4/listeners/JoinMessageListener.java
+++ src/main/java/me/js427/jsez4/listeners/JoinMessageListener.java
@@ -1,9 +1,21 @@
 package me.js427.jsez4.listeners;
 
+import me.js427.jsez4.JsEz4;
+import me.js427.jsez4.utils.ColorUtil;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinMessageListener implements Listener {
+    private final JsEz4 jsEz4 = new JsEz4();
+
     public void setJoinMessage(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        String message = jsEz4.getConfig().getString("chat.join-leave-message.custom-join-message");
+
+        message.replace("[USERNAME]", player.getName());
+
+        event.setJoinMessage(ColorUtil.translateAlternativeColorCodes(message));
     }
 }
---
+++ src/main/java/me/js427/jsez4/listeners/LeaveMessageListener.java
@@ -0,0 +1,4 @@
+package me.js427.jsez4.listeners;
+
+public class LeaveMessageListener {
+}
